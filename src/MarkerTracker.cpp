d4a00980501aa81170a235dff2463c2e13a99fe7
suiauthon <carek.marko@gmail.com>
2016-08-23 11:09:58 +0200

    roll and pitch set to 0

--- src/MarkerTracker.cpp
+++ src/MarkerTracker.cpp
@@ -156,8 +156,8 @@ void MarkerTracker::ar_track_alvar_sub(const ar_track_alvar_msgs::AlvarMarkers::
             markerPosition[1] = marker.pose.pose.position.y;
             markerPosition[2] = marker.pose.pose.position.z;
 
-            markerOrientation[0] = marker.pose.pose.orientation.x;
-            markerOrientation[1] = marker.pose.pose.orientation.y;
+            markerOrientation[0] = marker.pose.pose.orientation.x*0;
+            markerOrientation[1] = marker.pose.pose.orientation.y*0;
             markerOrientation[2] = marker.pose.pose.orientation.z;
 
             getRotationTranslationMatrix(markerTRMatrix, markerOrientation, markerPosition);
