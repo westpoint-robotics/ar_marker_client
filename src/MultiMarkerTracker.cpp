23d15d9c9d4e45890dc835f331f0b105c91dc1d3
thaus <tomislav.haus@fer.hr>
2017-03-17 16:42:11 +0100

    Added source files for multi marker tracking.

---
+++ src/MultiMarkerTracker.cpp
@@ -0,0 +1,224 @@
+/*
+ * MarkerTracker.cpp
+ *
+ *  Created on: Mar 4, 2014
+ *      Author: thaus
+ */
+
+#include "MultiMarkerTracker.h"
+
+MultiMarkerTracker::MultiMarkerTracker() {
+    // TODO Auto-generated constructor stub
+
+    cam2UAV << 	0.0, -1.0, 0.0, -0.006, //-0.003 - ako se optitrack marker ne pomice
+                -1.0, 0.0, 0.0, 0.0231, //0.0231 - ako se optitrack marker ne pomice
+                0.0, 0.0, -1.0, -0.0815, //-0.1148 - ako se optitrack marker ne pomice
+                0.0, 0.0, 0.0, 1.0;
+                
+	  UAV2GlobalFrame << 1.0, 0.0, 0.0, 0.0,
+					   0.0, 1.0, 0.0, 0.0,
+					   0.0, 0.0, 1.0, 0.0,
+					   0.0, 0.0, 0.0, 1.0;
+
+    filt_const = 0.9;
+    markerPositionOld[0] = 0;
+    markerPositionOld[1] = 0;
+    markerPositionOld[2] = 0;
+    first_meas = 0;
+}
+
+MultiMarkerTracker::~MultiMarkerTracker() {
+	// TODO Auto-generated destructor stub
+}
+
+void MultiMarkerTracker::LoadParameters(std::string file)
+{
+  // First open .yaml file
+  YAML::Node config = YAML::LoadFile(file);
+
+  std::vector<double> cam2imu_vector;
+  cam2imu_vector = config["cam2imu"].as<std::vector<double> >();
+
+  cam2UAV << cam2imu_vector[0], cam2imu_vector[1], cam2imu_vector[2], cam2imu_vector[3], //-0.003 - ako se optitrack marker ne pomice
+             cam2imu_vector[4], cam2imu_vector[5], cam2imu_vector[6], cam2imu_vector[7], //0.0231 - ako se optitrack marker ne pomice
+             cam2imu_vector[8], cam2imu_vector[9], cam2imu_vector[10], cam2imu_vector[11], //-0.1148 - ako se optitrack marker ne pomice
+             cam2imu_vector[12], cam2imu_vector[13], cam2imu_vector[14], cam2imu_vector[15];
+
+}
+
+void MultiMarkerTracker::quaternion2euler(double *quaternion, double *euler)
+{
+  euler[0] = atan2(2 * (quaternion[0] * quaternion[1] + 
+    quaternion[2] * quaternion[3]), 1 - 2 * (quaternion[1] * quaternion[1]
+    + quaternion[2] * quaternion[2]));
+
+  euler[1] = asin(2 * (quaternion[0] * quaternion[2] -
+    quaternion[3] * quaternion[1]));
+
+  euler[2] = atan2(2 * (quaternion[0]*quaternion[3] +
+    quaternion[1]*quaternion[2]), 1 - 2 * (quaternion[2]*quaternion[2] +
+    quaternion[3] * quaternion[3]));
+}
+
+void MultiMarkerTracker::getRotationTranslationMatrix(Eigen::Matrix4d &rotationTranslationMatrix,
+  double *orientationEuler, double *position)
+{
+	double r11, r12, r13, t1, r21, r22, r23, t2;
+	double r31, r32, r33, t3;
+
+	double x, y, z;
+
+  	x = orientationEuler[0];
+  	y = orientationEuler[1];
+  	z = orientationEuler[2];
+
+	r11 = cos(y)*cos(z);
+
+	r12 = cos(z)*sin(x)*sin(y) - cos(x)*sin(z);
+
+	r13 = sin(x)*sin(z) + cos(x)*cos(z)*sin(y);
+
+	r21 = cos(y)*sin(z);
+
+	r22 = cos(x)*cos(z) + sin(x)*sin(y)*sin(z);
+
+	r23 = cos(x)*sin(y)*sin(z) - cos(z)*sin(x);
+
+	r31 = -sin(y);
+
+	r32 = cos(y)*sin(x);
+
+	r33 = cos(x)*cos(y);
+
+	t1 = position[0];
+	t2 = position[1];
+	t3 = position[2];
+
+
+	rotationTranslationMatrix << r11, r12, r13, t1,
+	    	  					           r21, r22, r23, t2,
+	                             r31, r32, r33, t3,
+	                             0,   0,   0,   1;
+}
+
+void MultiMarkerTracker::getAnglesFromRotationTranslationMatrix(Eigen::Matrix4d &rotationTranslationMatrix,
+ double *angles)
+{
+  double r11, r21, r31, r32, r33;
+  double roll, pitch, yaw;
+
+  r11 = rotationTranslationMatrix(0,0);
+  r21 = rotationTranslationMatrix(1,0);
+  r31 = rotationTranslationMatrix(2,0);
+  r32 = rotationTranslationMatrix(2,1);
+  r33 = rotationTranslationMatrix(2,2);
+
+  roll = atan2(r32, r33);
+  pitch = atan2(-r31, sqrt(r32*r32 + r33*r33));
+  yaw = atan2(r21, r11);
+
+  angles[0] = roll;
+  angles[1] = pitch;
+  angles[2] = yaw;
+}
+
+void MultiMarkerTracker::odometryCallback(const nav_msgs::Odometry &msg)
+{
+	qGlobalFrame[1] = msg.pose.pose.orientation.x;
+	qGlobalFrame[2] = msg.pose.pose.orientation.y;
+	qGlobalFrame[3] = msg.pose.pose.orientation.z;
+	qGlobalFrame[0] = msg.pose.pose.orientation.w;
+	positionGlobalFrame[0] = msg.pose.pose.position.x;
+	positionGlobalFrame[1] = msg.pose.pose.position.y;
+	positionGlobalFrame[2] = msg.pose.pose.position.z;
+
+	quaternion2euler(qGlobalFrame, eulerGlobalFrame);
+
+	getRotationTranslationMatrix(UAV2GlobalFrame, eulerGlobalFrame, positionGlobalFrame);
+
+}
+
+void MultiMarkerTracker::ar_track_alvar_sub(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
+	int i;
+    ar_track_alvar_msgs::AlvarMarker marker;
+
+  bool packageDetectedFlag = false;
+  for(i = 0; i < msg->markers.size(); i++) {
+          marker = msg->markers[i];
+  /*
+  if (marker.id == usv_id) {
+                  ROS_INFO("USV detected");
+                  pub_usv_pose.publish(marker.pose.pose);
+          }
+  */
+  if (marker.id == target_id) {
+      ROS_INFO("target detected");
+      double q_marker[4], euler_marker[3];
+      std_msgs::Int16 detection_flag_msg;
+
+      markerPosition[0] = marker.pose.pose.position.x;
+      markerPosition[1] = marker.pose.pose.position.y;
+      markerPosition[2] = marker.pose.pose.position.z;
+
+      q_marker[1] = marker.pose.pose.orientation.x;
+      q_marker[2] = marker.pose.pose.orientation.y;
+      q_marker[3] = marker.pose.pose.orientation.z;
+      q_marker[0] = marker.pose.pose.orientation.w;
+
+      quaternion2euler(q_marker, euler_marker);
+
+      markerOrientation[0] = euler_marker[0];
+      markerOrientation[1] = euler_marker[1];
+      markerOrientation[2] = euler_marker[2];
+
+      getRotationTranslationMatrix(markerTRMatrix, markerOrientation, markerPosition);
+      markerGlobalFrame = UAV2GlobalFrame * cam2UAV * markerTRMatrix;
+
+      getAnglesFromRotationTranslationMatrix(markerGlobalFrame, markerOrientation);
+
+      marker.pose.pose.position.x = markerGlobalFrame(0,3);
+      marker.pose.pose.position.y = markerGlobalFrame(1,3);
+      marker.pose.pose.position.z = markerGlobalFrame(2,3);
+
+      marker.pose.pose.position.x += (markerOffset[0])*cos(markerOrientation[2]) - (markerOffset[1])*sin(markerOrientation[2]);
+      marker.pose.pose.position.y +=  (markerOffset[0])*sin(markerOrientation[2]) + (markerOffset[1])*cos(markerOrientation[2]);
+      marker.pose.pose.position.z += markerOffset[2];
+
+      marker.pose.pose.orientation.x = markerOrientation[0];
+      marker.pose.pose.orientation.y = markerOrientation[1];
+      marker.pose.pose.orientation.z = markerOrientation[2];
+
+      marker.pose.pose.orientation.w = 0;
+
+      marker.pose.header.stamp = ros::Time::now();
+      pub_target_pose.publish(marker.pose);
+      detection_flag_msg.data = 1;
+      pubDetectionFlag.publish(detection_flag_msg);
+      packageDetectedFlag = true;
+
+      if (first_meas == 0) {
+          first_meas = 1;
+          markerPositionOld[0] = marker.pose.pose.position.x;
+          markerPositionOld[1] = marker.pose.pose.position.y;
+          markerPositionOld[2] = marker.pose.pose.position.z;
+      }
+
+      marker.pose.pose.position.x = filt_const * markerPositionOld[0] + (1-filt_const) * marker.pose.pose.position.x;
+      marker.pose.pose.position.y = filt_const * markerPositionOld[1] + (1-filt_const) * marker.pose.pose.position.y;
+      marker.pose.pose.position.z = filt_const * markerPositionOld[2] + (1-filt_const) * marker.pose.pose.position.z;
+      pub_target_pose_f.publish(marker.pose);
+      markerPositionOld[0] = marker.pose.pose.position.x;
+      markerPositionOld[1] = marker.pose.pose.position.y;
+      markerPositionOld[2] = marker.pose.pose.position.z;
+  }
+          else {
+                  ROS_INFO("Detected unexpected marker id = %d", marker.id);
+          }
+  }
+  if(packageDetectedFlag==false)
+  {
+    std_msgs::Int16 detection_flag_msg;
+    detection_flag_msg.data = 0;
+    pubDetectionFlag.publish(detection_flag_msg);
+  }
+}
---
+++ src/MultiMarkerTracker.h
@@ -0,0 +1,100 @@
+/*
+ * MarkerTracker.h
+ *
+ *  Created on: Mar 4, 2014
+ *      Author: thaus
+ */
+
+#ifndef MULTIMARKERTRACKER_H_
+#define MULTIMARKERTRACKER_H_
+
+#include "ros/ros.h"
+#include "ar_track_alvar_msgs/AlvarMarker.h"
+#include "ar_track_alvar_msgs/AlvarMarkers.h"
+#include <nav_msgs/Odometry.h>
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "std_msgs/Int16.h"
+#include <eigen3/Eigen/Eigen>
+#include <dynamic_reconfigure/server.h>
+#include <ar_marker_client/MarkerOffsetConfig.h>
+#include "yaml-cpp/yaml.h"
+ #include <ros/package.h>
+
+class MultiMarkerTracker {
+public:
+	MultiMarkerTracker();
+	virtual ~MultiMarkerTracker();
+    void ar_track_alvar_sub(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg);
+    void odometryCallback(const nav_msgs::Odometry &msg);
+    void quaternion2euler(double *quaternion, double *euler);
+    void getRotationTranslationMatrix(Eigen::Matrix4d &rotationTranslationMatrix,
+    	double *orientationEuler, double *position);
+    void getAnglesFromRotationTranslationMatrix(Eigen::Matrix4d &rotationTranslationMatrix,
+    	double *angles);
+    void LoadParameters(std::string file);
+
+    //dynamic_reconfigure::Server<marker_tracker::MarkerOffsetConfig>::CallbackType params_call;
+
+	void setPubTargetPose(ros::Publisher pubTargetPose) {
+		pub_target_pose = pubTargetPose;
+	}
+
+    void setPubTargetPose_f(ros::Publisher pubTargetPose_f) {
+        pub_target_pose_f = pubTargetPose_f;
+    }
+
+    void setPubDetectionFlag(ros::Publisher fPub)
+    {
+    	pubDetectionFlag = fPub;
+    }
+
+	int getTargetId() const {
+		return target_id;
+	}
+
+	void setTargetId(int targetId) {
+		target_id = targetId;
+	}
+
+    void setMarkerOffset(double *offset) {
+        markerOffset[0] = offset[0];
+        markerOffset[1] = offset[1];
+        markerOffset[2] = offset[2];
+    }
+
+    void setMarkerIds(std::vector<int> marker_ids) {
+        this->marker_ids = marker_ids;
+    }
+
+    /*
+	void setPubUsvPose(ros::Publisher pubUsvPose) {
+		pub_usv_pose = pubUsvPose;
+	}
+
+	int getUsvId() const {
+		return usv_id;
+	}
+
+	void setUsvId(int usvId) {
+		usv_id = usvId;
+	}
+    */
+
+    int first_meas;
+    //int usv_id;
+    int target_id;
+    std::vector<int> marker_ids;
+    double qGlobalFrame[4], positionGlobalFrame[3], eulerGlobalFrame[3];
+    double markerPosition[3], markerOrientation[3];
+    double markerPositionOld[3];
+    double markerOffset[3];
+    double filt_const;
+	Eigen::Matrix4d cam2UAV, UAV2GlobalFrame, markerTRMatrix, markerGlobalFrame;
+    //ros::Publisher pub_usv_pose;
+	ros::Publisher pub_target_pose, pubDetectionFlag;
+    ros::Publisher pub_target_pose_f;
+
+};
+
+#endif /* MULTIMARKERTRACKER_H_ */
